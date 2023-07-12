e665d435a419619df8e5d42e965c8ca8715c7187
aafiya2004 <137600221+aafiya2004@users.noreply.github.com>
2023-07-12 09:48:20 +0530

    Add files via upload

---
+++ even  number.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int x = 2;
+        if(x%2==0) {
+            System.out.println("The number is even");
+            if(x%2==0) {
+                System.out.println("2");
+            }
+            else {
+                System.out.println("1");
+            }
+        }
+    }
+}
+
